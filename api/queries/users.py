0c6ef3802008cf815e59c7e3f819855e4bf7843a
Doruk Hacioglu <dorukhacioglu12@gmail.com>
2023-11-16 22:30:16 +0000

    Insert data into User database from endpoint

--- api/queries/users.py
+++ api/queries/users.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Union
+from queries.pool import pool
+
+class Error(BaseModel):
+    message: str
 
 class UserIn(BaseModel):
     first: str
@@ -11,3 +15,173 @@ class UserIn(BaseModel):
     goal: Optional[int]
     avatar_picture: Optional[str]
     bio: Optional[str]
+
+
+class UserOut(BaseModel):
+    first: str
+    last: str
+    username: str
+    email: str
+    location: str
+    goal: Optional[int]
+    avatar_picture: Optional[str]
+    bio: Optional[str]
+
+
+class UserQueries:
+    # def get_one(self, vacation_id: int) -> Optional[VacationOut]:
+    #     try:
+    #         # connect the database
+    #         with pool.connection() as conn:
+    #             # get a cursor (something to run SQL with)
+    #             with conn.cursor() as db:
+    #                 # Run our SELECT statement
+    #                 result = db.execute(
+    #                     """
+    #                     SELECT id
+    #                          , name
+    #                          , from_date
+    #                          , to_date
+    #                          , thoughts
+    #                     FROM vacations
+    #                     WHERE id = %s
+    #                     """,
+    #                     [vacation_id]
+    #                 )
+    #                 record = result.fetchone()
+    #                 if record is None:
+    #                     return None
+    #                 return self.record_to_vacation_out(record)
+    #     except Exception as e:
+    #         print(e)
+    #         return {"message": "Could not get that vacation"}
+
+    # def delete(self, vacation_id: int) -> bool:
+    #     try:
+    #         # connect the database
+    #         with pool.connection() as conn:
+    #             # get a cursor (something to run SQL with)
+    #             with conn.cursor() as db:
+    #                 db.execute(
+    #                     """
+    #                     DELETE FROM vacations
+    #                     WHERE id = %s
+    #                     """,
+    #                     [vacation_id]
+    #                 )
+    #                 return True
+    #     except Exception as e:
+    #         print(e)
+    #         return False
+
+    # def update(self, vacation_id: int, vacation: VacationIn) -> Union[VacationOut, Error]:
+    #     try:
+    #         # connect the database
+    #         with pool.connection() as conn:
+    #             # get a cursor (something to run SQL with)
+    #             with conn.cursor() as db:
+    #                 db.execute(
+    #                     """
+    #                     UPDATE vacations
+    #                     SET name = %s
+    #                       , from_date = %s
+    #                       , to_date = %s
+    #                       , thoughts = %s
+    #                     WHERE id = %s
+    #                     """,
+    #                     [
+    #                         vacation.name,
+    #                         vacation.from_date,
+    #                         vacation.to_date,
+    #                         vacation.thoughts,
+    #                         vacation_id
+    #                     ]
+    #                 )
+    #                 # old_data = vacation.dict()
+    #                 # return VacationOut(id=vacation_id, **old_data)
+    #                 return self.vacation_in_to_out(vacation_id, vacation)
+    #     except Exception as e:
+    #         print(e)
+    #         return {"message": "Could not update that vacation"}
+
+    # def get_all(self) -> Union[Error, List[VacationOut]]:
+    #     try:
+    #         # connect the database
+    #         with pool.connection() as conn:
+    #             # get a cursor (something to run SQL with)
+    #             with conn.cursor() as db:
+    #                 # Run our SELECT statement
+    #                 result = db.execute(
+    #                     """
+    #                     SELECT id, name, from_date, to_date, thoughts
+    #                     FROM vacations
+    #                     ORDER BY from_date;
+    #                     """
+    #                 )
+    #                 # result = []
+    #                 # for record in db:
+    #                 #     vacation = VacationOut(
+    #                 #         id=record[0],
+    #                 #         name=record[1],
+    #                 #         from_date=record[2],
+    #                 #         to_date=record[3],
+    #                 #         thoughts=record[4],
+    #                 #     )
+    #                 #     result.append(vacation)
+    #                 # return result
+
+    #                 return [
+    #                     self.record_to_vacation_out(record)
+    #                     for record in result
+    #                 ]
+    #     except Exception as e:
+    #         print(e)
+    #         return {"message": "Could not get all vacations"}
+
+    def create(self, user: UserIn) -> Union[UserOut, Error]:
+        try:
+            # connect the database
+            with pool.connection() as conn:
+                # get a cursor (something to run SQL with)
+                with conn.cursor() as db:
+                    # Run our INSERT statement
+                    result = db.execute(
+                        """
+                        INSERT INTO users
+                            (first, last, username, password, email, location, goal, avatar_picture, bio)
+                        VALUES
+                            (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                        RETURNING id;
+                        """,
+                        [
+                            user.first,
+                            user.last,
+                            user.username,
+                            user.password,
+                            user.email,
+                            user.location,
+                            user.goal,
+                            user.avatar_picture,
+                            user.bio
+                        ]
+                    )
+                    id = result.fetchone()[0]
+                    # Return new data
+                    # old_data = vacation.dict()
+                    # return VacationOut(id=id, **old_data)
+                    return self.user_in_to_out(id, user)
+        except Exception:
+            return {"message": "Create did not work"}
+
+    def user_in_to_out(self, id: int, user: UserIn):
+        old_data = user.dict()
+        return UserOut(id=id, **old_data)
+
+    # def record_to_vacation_out(self, record):
+    #     return VacationOut(
+    #         id=record[0],
+    #         name=record[1],
+    #         from_date=record[2],
+    #         to_date=record[3],
+    #         thoughts=record[4],
+    #     )
--- api/routers/users.py
+++ api/routers/users.py
@@ -1,10 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Response
+from typing import List, Optional, Union
 from queries.users import UserIn
+from queries.users import (
+    Error,
+    UserIn,
+    UserOut,
+    UserQueries,
+)
+
 
 router = APIRouter()
 
 
 @router.post("/users")
-def create_user(user: UserIn):
-    print('user', user)
-    return user
+def create_user(
+    user: UserIn,
+    repo: UserQueries = Depends()
+):
+    return repo.create(user)
